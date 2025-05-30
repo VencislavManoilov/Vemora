9f44dfabf9a4d1d13832222aa9f8702c2c4ebe06
VencislavManoilov <vencislav2.manoilov@gmail.com>
2025-05-30 11:18:03 +0300

    📄 Made it tokenize the files

--- main.c
+++ main.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX_SIZE 100
+
+void tokenize(const char *input) {
+    int i = 0;
+    while (input[i] != '\0') {
+        if (isspace(input[i])) {
+            i++;
+        } else if (isalpha(input[i])) {
+            char word[50];
+            int j = 0;
+            while (isalnum(input[i])) {
+                word[j++] = input[i++];
+            }
+            word[j] = '\0';
+
+            if (strcmp(word, "let") == 0) {
+                printf("[LET] ");
+            } else if (strcmp(word, "print") == 0) {
+                printf("[PRINT] ");
+            } else {
+                printf("[IDENTIFIER(%s)] ", word);
+            }
+        } else if (isdigit(input[i])) {
+            // Read number
+            char number[20];
+            int j = 0;
+            while (isdigit(input[i])) {
+                number[j++] = input[i++];
+            }
+            number[j] = '\0';
+            printf("[NUMBER(%s)] ", number);
+        } else {
+            // Single-character tokens
+            switch (input[i]) {
+                case '=': printf("[EQUALS] "); break;
+                case '+': printf("[PLUS] "); break;
+                case '-': printf("[MINUS] "); break;
+                case '*': printf("[STAR] "); break;
+                case '/': printf("[SLASH] "); break;
+                case '(': printf("[LPAREN] "); break;
+                case ')': printf("[RPAREN] "); break;
+                case ';': printf("[SEMICOLON] "); break;
+                default:  printf("[UNKNOWN(%c)] ", input[i]); break;
+            }
+            i++;
+        }
+    }
+
+    printf("\n");
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -22,7 +73,9 @@ int main(int argc, char *argv[]) {
 
     data[n] = '\0';
 
-    printf("%s", data);
+    printf("Source Code:\n%s\n", data);
+    printf("\nTokens\n");
+    tokenize(data);
 
     fclose(file);
 
