3420827984a7feafb548c3f1edef4932a1161ca7
Vencislav Manoilov <vencislav2.manoilov@gmail.com>
2025-06-11 02:45:47 +0300

    🖥️ Made the compiler get args like --debug that shows the logs

--- main.c
+++ main.c
@@ -9,12 +9,23 @@
 #define MAX_SIZE 100
 
 int main(int argc, char *argv[]) {
+    bool onlyCompile = false;
+    bool debug = false;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--only-compile") == 0) {
+            onlyCompile = true;
+        } else if (strcmp(argv[i], "--debug") == 0) {
+            debug = true;
+        }
+    }
+
     if (argc < 2) {
         printf("No file selected\n");
         return 1;
     }
     
-    char *file_path = argv[1];
+    char *file_path = argv[argc - 1];
     
     // Now you can use file_path to open and process the file
     FILE *file = fopen(file_path, "r");
@@ -27,31 +38,32 @@ int main(int argc, char *argv[]) {
     int n = fread(data, 1, MAX_SIZE - 1, file);
 
     data[n] = '\0';
-    printf("\nSource Code:\n%s\n", data);
+    debug && printf("\nSource Code:\n%s\n", data);
     
-    printf("\nv v v\n");
+    debug && printf("\nv v v\n");
     
-    printf("\nTokens:\n");
+    debug && printf("\nTokens:\n");
     TokenArray* tokens = tokenize(data);
     for (int i = 0; i < tokens->count; i++) {
         Token token = tokens->tokens[i];
         char *token_str = token_to_string(token);
-        printf("%s ", token_str);
+        debug && printf("%s ", token_str);
         free(token_str);
     }
-    printf("\n");
+    debug && printf("\n");
     
-    printf("\nv v v\n");
+    debug && printf("\nv v v\n");
 
-    printf("\nAST:\n");
+    debug && printf("\nAST:\n");
     Parser *parser = parser_create(tokens);
     ASTNode *ast = parse_program(parser);
-    ast_print(ast, 0);
+    if(debug)
+        ast_print(ast, 0);
 
-    printf("\nv v v\n");
+    debug && printf("\nv v v\n");
 
     // Generate assembly
-    printf("\nGenerating assembly...\n");
+    debug && printf("\nGenerating assembly...\n");
     FILE *asm_file = fopen("output.asm", "w");
     if (asm_file == NULL) {
         printf("Error creating assembly file\n");
@@ -62,9 +74,9 @@ int main(int argc, char *argv[]) {
     codegen_generate(codegen, ast);
     fclose(asm_file);
     
-    printf("Assembly generated in output.asm\n");
+    debug && printf("Assembly generated in output.asm\n");
     // Compile the generated assembly
-    printf("Compiling the assembly...\n");
+    debug && printf("Compiling the assembly...\n");
     int nasm_result = system("nasm -f elf64 output.asm -o output.o");
     if (nasm_result != 0) {
         printf("Error running nasm command\n");
@@ -77,7 +89,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     
-    printf("Compilation successful. Executable created as 'output'\n");
+    debug && printf("Compilation successful. Executable created as 'output'\n");
 
     // Cleanup
     codegen_free(codegen);
